0b1c8b9658fd2dfdd0ac4097c0b2d49804c6e100
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 16:04:34 +0100

    Feat: Make values of interactive buttons fetched from object in APP.js

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Footer.css';
 
-export default function Footer({content}) {
+export default function Footer({content, method}) {
     return (
         <div className="footerSection">
             <div className="iconSection">
                 <img src={content.comment} alt=""></img>
                 <p></p>
-                <img src={content.retweet} alt=""></img>
-                <p>2</p>
-                <img src={content.like} alt=""></img>
-                <p>3</p>
+                <img src={content.retweet} onClick={method.onClick} alt=""></img>
+                <p>{content.retweetValue}</p>
+                <img src={content.like} onClick={method.onClick} alt=""></img>
+                <p>{content.likeValue}</p>
                 <img src={content.message} alt=""></img>
                 
             </div>
