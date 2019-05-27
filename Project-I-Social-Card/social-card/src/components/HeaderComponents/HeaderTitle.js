4257a38d7a7ec4c29a525716618ce7935a5bb90c
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 14:03:35 +0100

    Feat: Get current date with momentjs

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Moment from 'moment';
 import './Header.css';
+
+const today = Moment().format('D MMM').toLowerCase();
 
 
 export default function HeaderTitle() {
     return (
         <div className="headerTitle">
             <h1>Lambda School</h1>
-            <p>@LambdaSchool . 26jan</p>
+            <p>@LambdaSchool . {today}</p>
         </div>
     )
 }
