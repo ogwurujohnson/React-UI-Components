e4cbb208b69d4e7b629f563210c16971991dec4c
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 13:29:23 +0100

    Feat: Create component image Thumbnail for displaying the logo

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+export default function ImageThumbnail() {
+    return (
+        <div className="imageContainer">
+            <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt=""></img>
+        </div>
+    )
+}
