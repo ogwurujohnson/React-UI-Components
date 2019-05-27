7d2086c33510fbade56fd9a28d43cc9233be6546
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 13:28:30 +0100

    Feat: Create the header container which houses components like:  - Headertitle  - HeaderContent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageContainer from './ImageThumbnail';
+
+export default function HeaderContainer() {
+    return (
+        <div className="header">
+            <ImageContainer />
+
+            <div className="headerText">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    )
+}
