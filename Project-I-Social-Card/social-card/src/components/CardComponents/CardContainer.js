52765565599fbfcecc64f9d38599cd5d4564f750
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 13:26:24 +0100

    Feat: create the card container component whcih houses both the card banner component and card content container

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+export default function CardContainer() {
+    return (
+        <div className="cardSection">
+            <div className="cardWrapper">
+                <CardBanner />
+                <CardContent />
+            </div>
+        </div>
+    )
+}
