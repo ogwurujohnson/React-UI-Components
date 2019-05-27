cdfd001c628fe49bca16ed7699bb8455ddc51497
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 13:28:58 +0100

    Feat: Create component HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+
+export default function HeaderTitle() {
+    return (
+        <div className="headerTitle">
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool . 26jan</p>
+        </div>
+    )
+}
