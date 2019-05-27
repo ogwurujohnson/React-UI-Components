e2e6a5c7d1631ddfecccfcced5774a4ac737e287
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 13:27:01 +0100

    Feat: Create component for our cardcomponent

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+export default function CardContent() {
+  return (
+    <div className="cardContent">
+      <h1>Get started with React</h1>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        or each state in your application
+      </p>
+      <i>reactjs.org</i>
+    </div>
+  );
+}
