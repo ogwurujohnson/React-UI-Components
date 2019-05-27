1bc8e404e6e20bde8c138b6ccfe85859d9eadeb9
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-27 13:28:44 +0100

    Feat: create component Header content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+export default function HeaderContent() {
+  return (
+    <div className="headerContent">
+      <p>
+        Let’s learn React by building simple interfaces with components, Don’t
+        to overthink it, just keep it simple and have fun. Once you are
+        comfortable using components you are well on your way mastering React
+      </p>
+    </div>
+  );
+}
