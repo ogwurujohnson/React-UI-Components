68d6711da3621f2869ea69a0750b1703931e497e
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-28 12:24:08 +0100

    Feat: Build out App.js  - Import Calculator container  - Render the container to the DOM

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
+import Calculator from './container/calculator';
 import './App.css';
+import './container/calculator/Calculator.css';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
-};
 
-export default App;
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Welcome to React Calculator</h3>
+        <Calculator />
+      </div>
+    )
+  }
+}
